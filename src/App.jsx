23bd27f357c2f60b1ffa11f3cd98bc74e9c4b96c
Slorant <sutuslorant@gmail.com>
2022-10-29 09:14:15 +0200

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  )
+}
+
+export default App
