4515cafd73c43128938733d72b4f4d0869107ba2
Slorant <sutuslorant@gmail.com>
2023-02-11 20:47:37 +0100

    HuskyPedia table of content for mobile is good to go

--- src/components/learnpagecomp/LearnPage.jsx
+++ src/components/learnpagecomp/LearnPage.jsx
@@ -1,67 +1,170 @@
 import React from 'react'
 import { HashLink as Link } from 'react-router-hash-link'
 import Content from './Content'
+import { useState } from 'react'
+import { motion } from 'framer-motion'
 
 const LearnPage = () => {
+  const [showT, setShowT] = useState(false)
+
+  const links = [
+    { text: 'Characteristics', link: '#spec', main: true },
+    { text: 'Personality', link: '#pers', main: true },
+    { text: 'Husky care', link: '#care', main: true },
+    { text: 'Food', link: '#feed', main: false },
+    { text: 'Grooming', link: '#groom', main: false },
+    { text: 'Environment', link: '#env', main: false },
+    { text: 'Puppies', link: '#puppy', main: false },
+    { text: 'Exercise', link: '#exr', main: false },
+    { text: 'Training a husky', link: '#train', main: true },
+    { text: 'Potty', link: '#potty', main: false },
+    { text: 'Crate', link: '#crate', main: false },
+    { text: 'Leash', link: '#leash', main: false },
+    { text: 'Clicker', link: '#clicker', main: false },
+    { text: 'Sources', link: '#sources', main: true },
+  ]
+
+  /* // In the component create variants of your animation state
+  const variants = {
+    rotate: { translateX: 50, opacity: 0, transition: { duration: 0.4 } },
+    // You can do whatever you want here, if you just want it to stop completely use `rotate: 0`
+    stop: {
+      opacity: 1,
+      translateX: -60,
+      transition: { duration: 0.4 } /* transition: { repeat: Infinity, repeatDelay: 3 } ,
+    },
+  } */
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const itemVariants = {
+    open: {
+      opacity: 1,
+      y: 0,
+      transition: { type: 'spring', stiffness: 300, damping: 24 },
+    },
+    closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
+  }
+
   return (
     <div className=" h-full w-full bg-cream">
-      <div className=" mt-14  flex w-full flex-col items-center  justify-center bg-cream xl:mt-16 ">
+      <motion.div
+        className=" mt-14  flex w-full flex-col items-center  justify-center bg-cream xl:mt-16 "
+        initial={'closed'}
+        animate={showT ? 'open' : 'closed'}>
         <h1 className=" mb-6 mt-14 font-header text-5xl  text-blue ">Huskypedia</h1>
-        <div className="invisible fixed top-52 left-32 flex h-[500px] w-80 flex-col items-center justify-center rounded-md bg-white 2xl:visible">
-          <h2 className=" font-headersc text-3xl text-blue">Table of contents</h2>
-          <ul className="mt-4 flex flex-col gap-2 font-header text-xl text-blue">
-            <li>
-              <Link to="#spec"> Characteristics</Link>
-            </li>
-            <li>
-              <Link to="#pers"> Personality</Link>
-            </li>
-            <li>
-              <Link to="#care"> Husky care</Link>
-            </li>
-            <ul>
-              <li className="ml-5 text-base">
-                <Link to="#feed"> Food</Link>
-              </li>
-              <li className="ml-5 text-base">
-                <Link to="#groom"> Grooming</Link>
-              </li>
-              <li className="ml-5 text-base">
-                <Link to="#env"> Environment and children</Link>
-              </li>
 
-              <li className="ml-5  text-base">
-                <Link to="#puppy"> Puppies</Link>
-              </li>
-              <li className="ml-5 text-base">
-                <Link to="#exr"> Exercise</Link>
-              </li>
-            </ul>
-            <li>
-              <Link to="#train"> Training a husky</Link>
-            </li>
-            <ul>
-              <li className="ml-5 text-base">
-                <Link to="#potty"> Potty</Link>
-              </li>
-              <li className="ml-5 text-base">
-                <Link to="#crate"> Crate</Link>
-              </li>
-              <li className="ml-5 text-base">
-                <Link to="#leash"> Leash</Link>
-              </li>
-              <li className="ml-5 text-base">
-                <Link to="#clicker"> Clicker</Link>
-              </li>
-            </ul>
-            <li className="mb-2">
-              <Link to="#sources"> Sources</Link>
-            </li>
-          </ul>
+        <motion.div
+          className={`${
+            showT ? '' : 'visible'
+          } fixed top-0 right-0 z-40 flex h-full w-64 flex-col items-center justify-center 
+            md:top-52 md:h-[500px]    2xl:visible`}
+          variants={{
+            open: {
+              boxShadow: '0px 0px 10px 5px rgba(0, 0, 0, 0.3)',
+              transition: {
+                type: 'spring',
+                bounce: 0,
+                duration: 0.5,
+                delay: 0.5,
+              },
+            },
+            closed: {
+              boxShadow: '0px 0px 0px 0px rgba(0, 0, 0, 0)',
+            },
+          }}>
+          {/* /* shadow-[0px_0px_10px_5px_rgba(0,0,0,0.3)] */
+          /* initial={{ opacity: 0 }}
+          animate={{ translateX: 40, opacity: 1, transition: { duration: 2 } }} */
+          /* variants=
+          {variants}
+        animate={showT ? 'rotate' : 'stop'} */}
+          <motion.ul
+            className="flex h-full w-full flex-col  justify-center   gap-2 bg-cream font-header text-2xl text-blue "
+            variants={{
+              open: {
+                clipPath: 'inset(0% 0% 0% 0% round 10px)',
+
+                transition: {
+                  type: 'spring',
+                  bounce: 0,
+                  duration: 0.7,
+                  delayChildren: 0.3,
+                  staggerChildren: 0.05,
+                },
+              },
+              closed: {
+                clipPath: 'inset(90% 50% 10% 50% round 10px)',
+
+                transition: {
+                  type: 'spring',
+                  bounce: 0,
+                  duration: 0.4,
+                },
+              },
+            }}
+            style={{ pointerEvents: showT ? 'auto' : 'none' }}>
+            <div className=" mb-20  flex flex-col  justify-center gap-2 ">
+              <div className=" flex w-full items-center justify-center">
+                <h2 className=" mb-4 font-headersc text-2xl text-blue">Table of contents</h2>
+              </div>
+              {links.map((link) => (
+                <motion.div
+                  variants={itemVariants}
+                  className="w-full cursor-pointer border-b-2 border-sand"
+                  onClick={() => {
+                    setShowT(false)
+                  }}>
+                  {/*   <TableOfContent link={link.link} text={link.text} main={link.main} /> */}
+                  <motion.li className={`${link.main ? 'ml-4' : 'ml-8 text-lg'} flex  w-full`}>
+                    <Link className=" w-full " to={link.link}>
+                      {' '}
+                      {link.text}
+                    </Link>
+                  </motion.li>
+                </motion.div>
+              ))}
+            </div>
+          </motion.ul>
+        </motion.div>
+        <div
+          className=" mt-4 flex h-full flex-col items-center justify-start rounded-md bg-white md:w-3/4 lg:w-2/3 xl:w-1/2 2xl:w-5/12"
+          onClick={() => {
+            setShowT(false)
+          }}>
+          <Content />
         </div>
 
-        <Content />
-      </div>
+        <motion.button
+          whileTap={{ scale: 0.9 }}
+          onClick={() => {
+            setShowT(!showT)
+          }}
+          className={`${
+            showT ? 'bg-blue' : 'bg-sand'
+          } uploadbutton fixed right-6 bottom-6 z-50  rounded-2xl  p-2
+           transition duration-300 ease-in-out hover:bg-blue md:right-16 md:bg-white lg:bottom-12 lg:right-14 2xl:hidden`}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon-tabler-file-upload icon icon-tabler icon-tabler-eye-table"
+            width="64"
+            height="64"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke={`${showT ? '#de9873' : '#2c3e50'}`}
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M4 3h16" />
+            <path d="M5 3v17a1 1 0 0 0 1 1h12a1 1 0 0 0 1 -1v-17" />
+            <path d="M14 7h-4" />
+            <path d="M10 15h4" />
+            <path d="M10 18h4" />
+            <path d="M12 11v-4" />
+          </svg>
+        </motion.button>
+      </motion.div>
     </div>
   )
 }
