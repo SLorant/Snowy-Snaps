8b9b67f6f08551a517e2649be538ee49e0a3c850
Slorant <sutuslorant@gmail.com>
2023-02-16 20:09:14 +0100

    Header design done but buggy

--- src/components/headercomp/Header.jsx
+++ src/components/headercomp/Header.jsx
@@ -4,10 +4,10 @@ import { doc, getDoc } from 'firebase/firestore'
 import { projectFirestore, projectStorage } from '../../../firebase/config'
 import { ref, getDownloadURL } from 'firebase/storage'
 import HeaderLink from './HeaderLink'
-import { useLocation, Link } from 'react-router-dom'
+import { useLocation, Link, useNavigate } from 'react-router-dom'
 import Lottie from 'lottie-react'
 import data from './data.json'
-import { HalfSun } from '@theme-toggles/react'
+import { motion } from 'framer-motion'
 
 const Header = () => {
   const { currentUser } = useAuth()
@@ -15,6 +15,15 @@ const Header = () => {
   const currentLocation = useLocation()
   const currLoc = currentLocation.pathname
   const lottieRef = useRef()
+  const navigate = useNavigate()
+  const itemVariants = {
+    open: {
+      opacity: 1,
+      y: 0,
+      transition: { type: 'spring', stiffness: 300, damping: 24 },
+    },
+    closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
+  }
   /*  const bodymovinOptions = {
     loop: true,
     autoplay: true,
@@ -100,45 +109,58 @@ const Header = () => {
         className={`
         ${showMenu ? 'h-full flex-col bg-sand' : 'h-16'}
         ${headerBg}
-      sticky flex  w-full  items-center justify-center  md:h-14 md:justify-between  xl:h-[72px]   `}>
-        <a
-          href="#"
+      sticky flex  w-full items-center justify-center   md:h-14 md:justify-between  xl:h-[72px]   `}
+        initial={'closed'}
+        animate={showMenu ? 'open' : 'open'}>
+        <Link
+          to="/"
           className={`${
             showMenu ? 'right-6' : 'left-2 hidden md:block '
           } absolute top-4 mb-1  w-40 font-headersc  text-lg font-bold
          text-blue  lg:left-6 lg:top-4 lg:w-52 lg:text-2xl xl:left-8 xl:left-6 xl:top-4 xl:w-64 xl:text-4xl`}>
           <img src="/src/assets/logo.png" alt="logo" />
-        </a>
+        </Link>
         <div className="">
           <button className="absolute top-1 left-2 w-12 md:hidden" onClick={handleClickMenu}>
             <Lottie autoplay={false} loop={false} lottieRef={lottieRef} animationData={data} />
           </button>
         </div>
 
-        <div
+        <motion.div
+          variants={{
+            open: {
+              clipPath: 'inset(0% 0% 0% 0% round 10px)',
+
+              transition: {
+                type: 'spring',
+                bounce: 0,
+                duration: 0.7,
+                delayChildren: 0.3,
+                staggerChildren: 0.05,
+              },
+            },
+            closed: {
+              clipPath: 'inset(90% 50% 10% 50% round 10px)',
+
+              transition: {
+                type: 'spring',
+                bounce: 0,
+                duration: 0.4,
+              },
+            },
+          }}
+          style={{ pointerEvents: showMenu ? 'auto' : 'none' }}
           className={`${
             showMenu
-              ? '  absolute top-48 h-2/3 w-full flex-col items-start justify-start  '
-              : 'hidden h-full w-full items-center justify-center md:flex'
-          }  mx-4  flex md:ml-40   md:flex md:flex-row lg:ml-60 xl:ml-80`}>
-          <div className=" mx-4 my-2 flex h-14 w-[90%] items-center justify-between rounded-md bg-cream">
-            <HeaderLink title="Home" location="/" currLoc={currLoc} showMenu={showMenu} />
-          </div>
-          <div className="mx-4 my-2 flex h-14 w-[90%] items-center justify-between rounded-md bg-cream ">
-            <HeaderLink title="Gallery" location="/watch" currLoc={currLoc} showMenu={showMenu} />
-          </div>
-          <div className="mx-4 my-2 flex h-14 w-[90%] items-center justify-between rounded-md bg-cream">
-            <HeaderLink
-              title="Huskypedia"
-              location="/learn"
-              currLoc={currLoc}
-              showMenu={showMenu}
-            />
-          </div>
-          <div className="mx-4 my-2 flex h-14 w-[90%] items-center justify-between rounded-md bg-cream  md:hidden">
+              ? 'absolute top-48 h-2/3 w-full flex-col items-start justify-start  '
+              : 'hidden h-full  items-center justify-center md:flex'
+          }  mx-4 flex  md:mx-0 md:ml-40   md:flex md:flex-row lg:ml-60 xl:ml-80`}>
+          <HeaderLink title="Home" location="/" currLoc={currLoc} showMenu={showMenu} />
+          <HeaderLink title="Gallery" location="/watch" currLoc={currLoc} showMenu={showMenu} />
+          <HeaderLink title="Huskypedia" location="/learn" currLoc={currLoc} showMenu={showMenu} />
+          <div className="w-full md:hidden">
             <HeaderLink title="Profile" location="/profile" currLoc={currLoc} showMenu={showMenu} />
           </div>
-
           {/*  <div className="md:hidden">
             <HeaderLink
               title="My Gallery"
@@ -147,7 +169,7 @@ const Header = () => {
               showMenu={showMenu}
             />
           </div>*/}
-          <div className="mx-4 my-2 flex h-14 w-[90%] items-center justify-between rounded-md bg-cream  md:hidden">
+          <div className="w-full md:hidden">
             <HeaderLink
               title="My Snaps"
               location={`/${username}/gallery`}
@@ -155,38 +177,40 @@ const Header = () => {
               showMenu={showMenu}
             />
           </div>
-
-          <div className="mx-4 mt-40 flex w-[91%] items-end justify-between">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="icon icon-tabler icon-tabler-moon"
-              width="60"
-              height="60"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="#2c3e50"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="icon icon-tabler icon-tabler-logout"
-              width="60"
-              height="60"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="#2c3e50"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
-              <path d="M7 12h14l-3 -3m0 6l3 -3" />
-            </svg>
-          </div>
+        </motion.div>
+        <div
+          className={`${
+            showMenu ? 'absolute bottom-6' : 'hidden'
+          }  md:hidden" mx-4 flex w-[91%] items-end justify-between`}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="icon icon-tabler icon-tabler-moon"
+            width="60"
+            height="60"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#2c3e50"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="icon icon-tabler icon-tabler-logout"
+            width="60"
+            height="60"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#2c3e50"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+            <path d="M7 12h14l-3 -3m0 6l3 -3" />
+          </svg>
         </div>
 
         {useAuth().currentUser ? (
@@ -197,16 +221,23 @@ const Header = () => {
                 : 'flex h-full w-full items-start justify-center md:w-auto'
             }  `}>
             <div className="flex w-3/4 flex-col items-end">
-              <div
+              <motion.div
                 className={`${
                   showMenu
-                    ? ' flex h-6 w-full   justify-end '
-                    : 'mr-16 flex hidden h-full w-full justify-end'
+                    ? ' flex h-8 w-full   justify-end '
+                    : 'mr-16 flex hidden h-full w-full justify-end md:block'
                 }  ${
                   username.length > 10 ? 'text-xl' : ' text-2xl'
-                }   md:block md:w-auto xl:mr-24`}>
-                <HeaderLink title={username} location="/profile" currLoc={currLoc} />
-              </div>
+                } md:h-14 md:w-auto    xl:mr-16 xl:h-[72px]`}>
+                <div className="hidden md:block">
+                  <HeaderLink title={username} location="/profile" currLoc={currLoc} />
+                </div>
+                <Link
+                  to="/profile"
+                  className={`${showMenu ? '' : 'hidden'} mr-2 font-header text-blue  md:hidden`}>
+                  {username}
+                </Link>
+              </motion.div>
               <Link
                 to="/profile"
                 className={`${showMenu ? '' : 'hidden'} mr-2 font-header text-sm  text-blue`}>
@@ -217,8 +248,8 @@ const Header = () => {
               <img
                 id="myimg"
                 className={`${
-                  showMenu ? ' h-14 w-14' : ' absolute right-2 top-2'
-                } h-12 w-12 rounded-full shadow-md md:top-[2px] md:h-11 md:w-11 lg:right-3 xl:right-5 xl:top-2 xl:h-14 xl:w-14`}
+                  showMenu ? ' h-14 w-14' : ' absolute right-2 top-2  h-12 w-12'
+                } rounded-full shadow-md md:top-[2px] md:h-12 md:h-11 md:w-12 md:w-11 lg:right-3 xl:right-5 xl:top-2 xl:h-14 xl:w-14`}
                 src="src\assets\profile.png"
                 alt="userpic"
               />
--- src/components/headercomp/HeaderLink.jsx
+++ src/components/headercomp/HeaderLink.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 const HeaderLink = ({ showMenu, title, location, currLoc }) => {
   const { pathname } = useLocation()
+  const navigate = useNavigate()
+
+  const handleClickButton = (url) => {
+    navigate(url)
+  }
+
+  const itemVariants = {
+    open: {
+      opacity: 1,
+      y: 0,
+      transition: { type: 'spring', stiffness: 300, damping: 24 },
+    },
+    closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
+  }
 
   return (
-    <Link
-      to={location}
-      className={`${pathname === location ? ' border-darkblue' : 'border-transparent'}
+    <motion.button
+      variants={itemVariants}
+      onClick={() => {
+        handleClickButton(location)
+      }}
+      className=" mx-4 my-2 flex h-14 w-[90%] items-center justify-between 
+    rounded-md bg-cream  md:m-auto md:h-14 md:w-auto md:bg-transparent xl:h-[72px]">
+      <Link
+        to={location}
+        className={`${pathname === location ? ' border-darkblue' : 'border-transparent'}
     ${location === '/profile' ? 'font-header' : 'font-header md:font-headersc'}
     ${
       currLoc === '/'
@@ -21,8 +43,9 @@ const HeaderLink = ({ showMenu, title, location, currLoc }) => {
     ${showMenu ? 'my-2 ml-4 h-14 border-none text-3xl tracking-wide' : ' h-full'}
      headerlink z-50 block flex items-center border-b-2 p-2 px-2 text-center  text-blue  transition  duration-500
               lg:px-4 lg:text-2xl    xl:px-6   xl:text-3xl`}>
-      {title}
-    </Link>
+        {title}
+      </Link>
+    </motion.button>
   )
 }
 
